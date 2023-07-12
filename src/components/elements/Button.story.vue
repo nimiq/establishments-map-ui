70fe8631e2adc68be507302e1b31cae7419b206e
maximo <maximo@nimiq.com>
2023-07-12 14:02:39 +0700

    Added histoire

---
+++ src/components/elements/Button.story.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import Button from "./Button.vue";
+</script>
+
+<template>
+  <Story title="Button">
+    <Variant title="default">
+      <Button bgColor="space" size="md">
+        <template #icon>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" class="w-3 h-3 text-white">
+            <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M1 8h14M8 1v14" />
+          </svg>
+        </template>
+
+        <template #label>
+          Show more establishments
+        </template>
+      </Button>
+    </Variant>
+    <Variant title="Icon">
+      <Button bgColor="white" size="lg">
+        <template #icon>
+          <svg viewBox="0 0 12 6" fill="none" xmlns="http://www.w3.org/2000/svg" class="w-3 text-space">
+            <path d="M10.5 1 6 5 1.5 1" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+              stroke-linejoin="round"></path>
+          </svg>
+        </template>
+      </Button>
+    </Variant>
+  </Story>
+</template>
