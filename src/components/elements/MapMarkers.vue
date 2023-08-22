171f13218af4f7a0658806c98970525bc4ab23d6
maximo <maximo@nimiq.com>
2023-08-22 22:49:14 +0700

    marker count fix

--- src/components/elements/MapMarkers.vue
+++ src/components/elements/MapMarkers.vue
@@ -13,7 +13,7 @@ const { clusters } = storeToRefs(useCluster())
   >
     <div v-if="count === 1" class="grid w-3 h-3 text-sm font-bold text-white rounded-full shadow ring-white/40 ring-2 place-content-center bg-space" />
     <div v-else class="grid text-sm font-bold text-white rounded-full shadow aspect-square place-content-center bg-space ring-white/20 ring-2 ring-offset-1 ring-offset-white/40" :style="`width: ${0.32 * count + 32}px`">
-      {{ count }}
+      {{ count < 100 ? count : '99+' }}
     </div>
   </CustomMarker>
 </template>
