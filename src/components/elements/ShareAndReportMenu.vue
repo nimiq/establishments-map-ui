a0a7fe27219250a6f4e74039c7f6ff0226b150d5
maximo <maximo@nimiq.com>
2023-08-30 16:15:14 +0700

    Removed abstraction of DotsMenu

--- src/components/elements/ShareAndReportMenu.vue
+++ src/components/elements/ShareAndReportMenu.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
 import { useShare } from '@vueuse/core'
-import { DropdownMenuItem } from 'radix-vue'
+import { DropdownMenuContent, DropdownMenuItem, DropdownMenuPortal, DropdownMenuRoot, DropdownMenuTrigger } from 'radix-vue'
 import type { PropType } from 'vue'
-import type { Location } from '@/types'
-import DotsMenu from '@/components/atoms/DotsMenu.vue'
 import IconFlag from '@/components/icons/icon-flag.vue'
 import IconShare from '@/components/icons/icon-share.vue'
+import IconThreeDots from '@/components/icons/icon-three-dots.vue'
 import { i18n } from '@/i18n/i18n-setup'
+import type { Location } from '@/types'
 
 const props = defineProps({
   location: {
@@ -21,26 +21,37 @@ const url = () => `${window.location.origin}/${window.location.pathname}?uuid=${
 </script>
 
 <template>
-  <DotsMenu v-bind="$attrs">
-    <DropdownMenuItem
-      v-if="shareIsSupported"
-      class="flex px-4 py-2 text-white transition-colors outline-none cursor-pointer select-none hover:text-white/80"
-      @click="share({
-        title: location.name,
-        text: i18n.t('Check out {locationName} on Nimiq\'s Crypto Map'),
-        url: url(),
-      })"
-    >
-      <IconShare class="w-4 h-4 mr-3" />
-      <span class="text-base font-semibold leading-4">Share</span>
-    </DropdownMenuItem>
-    <DropdownMenuItem
-      as="a"
-      :href="`/location/report?uuid=${location.uuid}`"
-      class="flex px-4 py-2 transition-colors outline-none cursor-pointer select-none text-salmon hover:text-salmon/80"
-    >
-      <IconFlag class="w-4 h-4 mr-3" />
-      <span class="text-base font-semibold leading-4">Report</span>
-    </DropdownMenuItem>
-  </DotsMenu>
+  <DropdownMenuRoot>
+    <DropdownMenuTrigger v-bind="$attrs">
+      <IconThreeDots class="w-5 h-5 text-space/30" />
+    </DropdownMenuTrigger>
+
+    <DropdownMenuPortal>
+      <DropdownMenuContent
+        class="outline-none bg-gradient-space rounded-sm p-1 will-change-[opacity] shadow"
+        :side-offset="10"
+      >
+        <DropdownMenuItem
+          v-if="shareIsSupported"
+          class="flex px-4 py-2 text-white transition-colors outline-none cursor-pointer select-none hover:text-white/80"
+          @click="share({
+            title: location.name,
+            text: i18n.t('Check out {locationName} on Nimiq\'s Crypto Map'),
+            url: url(),
+          })"
+        >
+          <IconShare class="w-4 h-4 mr-3" />
+          <span class="text-base font-semibold leading-4">Share</span>
+        </DropdownMenuItem>
+        <DropdownMenuItem
+          as="a"
+          :href="`/location/report?uuid=${location.uuid}`"
+          class="flex px-4 py-2 transition-colors outline-none cursor-pointer select-none text-salmon hover:text-salmon/80"
+        >
+          <IconFlag class="w-4 h-4 mr-3" />
+          <span class="text-base font-semibold leading-4">Report</span>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenuPortal>
+  </DropdownMenuRoot>
 </template>
---
+++ src/components/icons/icon-three-dots.vue
@@ -0,0 +1,3 @@
+<template>
+  <svg viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg"><g opacity=".3" fill="currentColor"><circle cx="10" cy="2" r="2" /><circle cx="10" cy="10" r="2" /><circle cx="10" cy="18" r="2" /></g></svg>
+</template>
