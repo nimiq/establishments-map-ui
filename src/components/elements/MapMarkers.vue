5daafed77f677888fe66512c61b51c0eb47b6742
maximo <maximo@nimiq.com>
2023-09-01 19:53:26 +0700

    Add hover and focus effect to markers

--- src/components/elements/MapMarkers.vue
+++ src/components/elements/MapMarkers.vue
@@ -37,7 +37,7 @@ function onClusterClick(center: Point, clusterId: number) {
   })
 }
 
-const showSingleName = () => zoom() >= 8
+const showSingleName = () => zoom() >= 11
 const showCategoryIcon = () => zoom() >= 13
 
 const CategoryIcon = defineAsyncComponent(
@@ -69,28 +69,31 @@ function extractColorFromBg(bg: string) {
     v-for="({ center, count, clusterId }) in clusters" :key="clusterId"
     :options="{ position: center, anchorPoint: 'CENTER' }"
   >
-    <div class="grid text-sm font-bold text-white rounded-full shadow cursor-pointer aspect-square place-content-center bg-space ring-white/20 ring-2 ring-offset-1 ring-offset-white/40" :style="`width: clamp(24px, ${0.24 * count + 24}px, 48px); font-size: clamp(14px, ${0.14 * count + 4}px, 18px)`" @click="onClusterClick(center, clusterId)">
+    <div class="grid text-sm font-bold text-white transition-colors rounded-full shadow cursor-pointer aspect-square place-content-center bg-space hover:bg-[#35395A] focus:bg-[#35395A] ring-white/20 ring-2 ring-offset-1 ring-offset-white/40" :style="`width: clamp(24px, ${0.24 * count + 24}px, 48px); font-size: clamp(14px, ${0.14 * count + 4}px, 18px)`" @click="onClusterClick(center, clusterId)">
       {{ count < 100 ? count : '99+' }}
     </div>
   </CustomMarker>
 
   <DefineTemplate v-slot="{ location: { category, name, isAtm, bg, uuid } }">
-    <div class="flex items-center gap-x-2 max-w-[176px]">
-      <div
-        v-if="isAtm" class="grid w-8 h-8 text-white rounded-full shadow ring-white/40 ring-2 place-content-center" :style="`background: ${
-          bg}`"
-      >
+    <div class="flex items-center gap-x-2 max-w-[176px] group/marker">
+      <div v-if="isAtm" class="grid w-8 h-8 text-white rounded-full shadow ring-white/40 ring-2 place-content-center" :style="`background: ${bg}`">
         {{ $t('ATM') }}
       </div>
-      <div v-else-if="showCategoryIcon()" class="grid w-8 h-8 text-white rounded-full shadow ring-white/40 ring-2 place-content-center" :class="uuid === selectedUuid ? 'bg-sky' : 'bg-space'">
+      <div
+        v-else-if="showCategoryIcon()"
+        class="grid w-8 h-8 text-white transition-colors rounded-full shadow ring-white/40 ring-2 place-content-center" :class="uuid === selectedUuid ? 'bg-sky' : 'bg-space group-hover/marker:bg-[#494d6c] group-focus/marker:bg-[#494d6c]'"
+      >
         <CategoryIcon :category="category" class="w-7" />
       </div>
-      <div v-else class="grid w-3 h-3 text-sm font-bold text-white rounded-full shadow ring-white/40 ring-2 place-content-center" :class="uuid === selectedUuid ? 'bg-sky' : 'bg-space'" />
+      <div
+        v-else class="grid w-3 h-3 text-sm font-bold text-white transition-colors rounded-full shadow ring-white/40 ring-2 place-content-center"
+        :class="uuid === selectedUuid ? 'bg-sky' : 'bg-space group-hover/marker:bg-[#494d6c] group-focus/marker:bg-[#494d6c]'"
+      />
       <!-- <PopoverAnchor class="mx-1" /> -->
       <span
         v-if="!isAtm && showSingleName()"
-        class="flex-1 text-base font-semibold leading-none text-left select-none outline-text"
-        :class="[uuid === selectedUuid ? 'text-sky' : 'text-space', { invisible: !isMobile && uuid === selectedUuid }]"
+        class="flex-1 text-base font-semibold leading-none text-left transition-[color,-webkit-text-stroke] select-none [-webkit-text-stroke:_3px_white] relative before:content-[attr(data-outline)] before:absolute before:[-webkit-text-stroke:0]"
+        :class="[uuid === selectedUuid ? 'text-sky' : 'text-space group-hover/marker:text-space/80 group-focus/marker:bg-[#35395A]', { invisible: !isMobile && uuid === selectedUuid }]"
         :data-outline="name"
       >
         {{ name }}
@@ -135,18 +138,3 @@ function extractColorFromBg(bg: string) {
     </PopoverRoot>
   </CustomMarker>
 </template>
-
-<style scoped>
-.outline-text {
-  -webkit-text-stroke: 3px white;
-  text-stroke: 3px white;
-  position: relative;
-}
-
-.outline-text::before {
-  content: attr(data-outline);
-  position: absolute;
-  -webkit-text-stroke: 0;
-  text-stroke: 0;
-}
-</style>
