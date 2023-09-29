314771c5926241702a99ff77d6897fbead62286e
maximo <maximo@nimiq.com>
2023-09-29 19:45:03 +0700

    Removed dynamic og image

--- supabase/functions/og-image/index.ts
+++
@@ -1,7 +0,0 @@
-import { serve } from 'https://deno.land/std@0.168.0/http/server.ts'
-import handler from './handler.tsx'
-
-// For development run:
-// deno task og:dev
-// Go to http://localhost:54321/functions/v1/og-image
-serve(handler)
