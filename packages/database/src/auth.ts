9ed6fe9dc57f9831d109b8d152702aa0bfb3464d
Maxi <maximogarciamtnez@gmail.com>
2024-05-14 13:36:16 +0200

    fix auth

--- packages/database/src/auth.ts
+++ packages/database/src/auth.ts
@@ -31,19 +31,12 @@ export async function authenticateAnonUser({ apikey, url }: DatabaseArgs, captch
   }
   const data: { captcha_uuid: string } = await response.json()
   if (!data || !data.captcha_uuid) {
-    if (typeof crypto === 'undefined')
-      var crypto = require('crypto');
-    if (!('randomUUID' in crypto))
-      // https://stackoverflow.com/a/2117523/2800218
-      // LICENSE: https://creativecommons.org/licenses/by-sa/4.0/legalcode
-      crypto.randomUUID = function randomUUID() {
-        return (
-          // @ts-expect-error Thi is fine
-          [1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g,
-            c => (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
-          );
-      };
-    data.captcha_uuid = crypto.randomUUID()
+    let randomUUID = globalThis.crypto?.randomUUID || (
+      // @ts-expect-error This is fine
+      [1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g,
+        c => (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+      )
+    data.captcha_uuid = randomUUID()
   }
   //   throw new Error('No captcha uuid found!')
 
