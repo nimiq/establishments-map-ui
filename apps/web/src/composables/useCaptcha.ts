ee9983a7a4a7e435013641382c86ba12dbd30053
Alberto Monterroso <14013679+Albermonte@users.noreply.github.com>
2024-06-18 10:27:45 +0300

    Prevent multiple request to captcha server This was causing the server to return an error for duplicated token, making the app fail to load

--- apps/web/src/composables/useCaptcha.ts
+++ apps/web/src/composables/useCaptcha.ts
@@ -1,11 +1,15 @@
 import { authenticateAnonUser } from 'database'
+import { createSharedComposable } from '@vueuse/core'
 import { DATABASE_ARGS } from '@/shared'
 
 const CAPTCHA_TOKEN_VALIDITY = 10 * 60 * 1000 // 10 minutes for the captcha token
 const RECAPTCHA_SITE_KEY = import.meta.env.VITE_RECAPTCHA_SITE_KEY
 
-export function useCaptcha() {
+function _useCaptcha() {
   const token = ref<string>()
+  const captchaUuid = ref<string>()
+  const lock = ref(Promise.resolve()) // Lock to prevent multiple requests
+
   async function getCaptchaToken() {
     while (!globalThis.grecaptcha || !globalThis.grecaptcha.execute)
       await new Promise(resolve => setTimeout(resolve, 100))
@@ -24,18 +28,33 @@ export function useCaptcha() {
   }
 
   async function getAsyncValue(): Promise<string> {
+    // Wait for the previous request to finish, if any
+    await lock.value
+    // Lock the execution by replacing the lock promise with a new, unresolved promise.
+    let unlock
+    lock.value = new Promise(resolve => unlock = resolve)
+
+    if (captchaUuid.value)
+      return captchaUuid.value
+
     try {
       if (!token.value)
         await getCaptchaToken()
-      return await authenticateAnonUser(DATABASE_ARGS, token.value!)
+      captchaUuid.value = await authenticateAnonUser(DATABASE_ARGS, token.value!)
+      return captchaUuid.value
     }
     catch (error: any) {
       if ('message' in error && error.message.includes('Invalid Captcha UUID')) {
         globalThis.localStorage.removeItem('cryptomap__captcha_token_uuid')
         token.value = undefined
+        console.error('Invalid captcha token, retrying...', error)
         return await getAsyncValue()
       }
       throw error
+    }
+    finally {
+      // Release the lock for subsequent operations and reset the lock state.
+      unlock!()
     }
   }
 
@@ -70,3 +89,5 @@ export function useCaptcha() {
     init,
   }
 }
+
+export const useCaptcha = createSharedComposable(_useCaptcha)
