0b37f5b68231dc6d3d98b61d1e4f88b59d6bb6f2
Maxi <maximogarciamtnez@gmail.com>
2024-06-21 17:10:33 +0300

    feat(user-location): make accuracy circle dynamic

--- apps/web/src/components/UserLocationMarker.vue
+++ apps/web/src/components/UserLocationMarker.vue
@@ -1,12 +1,23 @@
 <script lang="ts" setup>
-const props = defineProps<{ accuracy: number, zoom: number }>()
+import { EstimatedMapPosition } from "types";
+import { metersToPx } from "../../../../packages/geo/src/geo-utils.js";
 
-const styleObject = reactive({ '--radius': '64px' })
+const props = defineProps<{ browserPosition: EstimatedMapPosition }>();
 
-watch(() => props.accuracy, () => styleObject['--radius'] = `${Math.min(64, Math.max(24, props.accuracy))}px`, { immediate: true })
+const { zoom } = storeToRefs(useMap())
+const accuracyCircleEl = ref();
+const radius = useCssVar("--radius", accuracyCircleEl, { initialValue: "64px" });
+
+watch([props.browserPosition, zoom], () => {
+  const pixelRadius = metersToPx(props.browserPosition.accuracy, zoom.value, props.browserPosition.center.lat);
+  radius.value = `${Math.min(zoom.value < 11 ? 0 : 64, Math.max(24, pixelRadius))}px`;
+}, { immediate: true });
+
 </script>
 
 <template>
-  <div rounded-full bg-blue-300 op-60 size="$radius" :style="styleObject" />
-  <div absolute size-10 rounded-full bg-blue ring="2 white" top="50%" left="50%" transform="translate--50%" />
+  <div grid="~ cols-1 rows-1" pointer-events-none>
+    <div size-10 rounded-full bg-blue ring="2 white" col-span-full row-span-full self-center justify-self-center z-1 />
+    <div rounded-full bg-blue-300 op-60 size="$radius" transition-all duration-400 col-span-full row-span-full ref="accuracyCircleEl" />
+  </div>
 </template>
