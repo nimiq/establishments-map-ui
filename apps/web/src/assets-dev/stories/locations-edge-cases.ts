1ebea1e8921873588e6df235ffa4f4be52909dd7
Maxi <maximogarciamtnez@gmail.com>
2024-06-27 18:21:11 +0300

    chore: comment out unnecessary file

--- apps/web/src/assets-dev/stories/locations-edge-cases.ts
+++ apps/web/src/assets-dev/stories/locations-edge-cases.ts
@@ -1,18 +1,18 @@
-import { Currency, type MapLocation, Provider } from 'types'
-import { getExtra } from './locations'
+// import { Currency, type MapLocation, Provider } from 'types'
+// import { getExtra } from './locations'
 
-export const locations: Partial<Record<Provider, MapLocation>> = {
-  [Provider.DefaultAtm]: {
-    uuid: '1',
-    name: 'ATM with no rating',
-    address: 'This is suppose a long address. This is weird case since ',
-    accepts: [Currency.BTC, Currency.NIM, Currency.ETH],
-    gmaps_types: ['stadium'],
-    lat: 1,
-    lng: 1,
-    cryptocity: 'San Jose',
-    gmaps: 'https://goo.gl/maps/ujJkv9DFuPfkwqat9',
-    photo: 'https://images.unsplash.com/photo-1646491946169-76e0668b8b3f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=735&q=80',
-    ...getExtra(Provider.DefaultAtm, [Currency.BTC, Currency.NIM, Currency.ETH]),
-  },
-}
+// export const locations: Partial<Record<Provider, MapLocation>> = {
+//   [Provider.DefaultAtm]: {
+//     uuid: '1',
+//     name: 'ATM with no rating',
+//     address: 'This is suppose a long address. This is weird case since ',
+//     accepts: [Currency.BTC, Currency.NIM, Currency.ETH],
+//     gmaps_types: ['stadium'],
+//     lat: 1,
+//     lng: 1,
+//     cryptocity: 'San Jose',
+//     gmaps: 'https://goo.gl/maps/ujJkv9DFuPfkwqat9',
+//     photo: 'https://images.unsplash.com/photo-1646491946169-76e0668b8b3f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=735&q=80',
+//     ...getExtra(Provider.DefaultAtm, [Currency.BTC, Currency.NIM, Currency.ETH]),
+//   },
+// }
