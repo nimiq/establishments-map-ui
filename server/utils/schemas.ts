9b8d252931395eac70d7a355e357f653b69481ce
Max <maximo@nimiq.com>
2024-10-16 11:42:43 +0200

    Update schemas.ts

--- server/utils/schemas.ts
+++ server/utils/schemas.ts
@@ -1,9 +1,9 @@
-import { maxValue, minValue, number, object, pipe, string, uuid, coerce } from 'valibot'
+import { maxValue, minValue, number, object, pipe, string, uuid, unknown, transform } from 'valibot'
 
 export const UuidSchema = object({ uuid: pipe(string(), uuid('The UUID is badly formatted')) })
 
-const LatSchema = pipe(coerce(string(), number()),, minValue(-90, 'Latitude must be greater than or equal to -90'), maxValue(90, 'Latitude must be less than or equal to 90'))
-const LngSchema = pipe(coerce(string(), number()),, minValue(-180, 'Longitude must be greater than or equal to -180'), maxValue(180, 'Longitude must be less than or equal to 180'))
+const LatSchema = pipe(unknown(), transform(Number), minValue(-90, 'Latitude must be greater than or equal to -90'), maxValue(90, 'Latitude must be less than or equal to 90'))
+const LngSchema = pipe(unknown(), transform(Number), minValue(-180, 'Longitude must be greater than or equal to -180'), maxValue(180, 'Longitude must be less than or equal to 180'))
 export const BoundingBoxSchema = object({ nelat: LatSchema, nelng: LngSchema, swlat: LatSchema,  swlng: LngSchema })
 
 export const ZoomSchema = pipe(
