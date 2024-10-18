4128c663f8a4f43813724794c1e98baf612b6041
onmax <maximogarciamtnez@gmail.com>
2024-10-18 18:03:44 +0200

    fix head

--- server/routes/images/[...pathname].get.ts
+++ server/routes/images/[...pathname].get.ts
@@ -7,7 +7,7 @@ export default eventHandler(async (event) => {
   // if the request is for a location image, serve it from blob storage
   if (pathname.startsWith('location:')) {
     // if the location is not cached, fetch it from the KV store
-    if (!hubKV().has(pathname) || !hubBlob().head(pathname)) {
+    if (!hubKV().has(pathname) || !(await hubBlob().head(pathname))) {
       consola.info(`Location ${pathname} not cached, fetching from KV store`)
       const location = await getLocation(event, pathname)
       await cacheLocation(location)
